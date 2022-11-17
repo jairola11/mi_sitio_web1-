822472ba151555eb2de7d4f21c6d7c73afb5ad90
jairola11 <jairostivenla11@gmail.com>
2022-11-17 11:42:36 -0500

    menu

---
+++ js/media-query.js
@@ -0,0 +1,14 @@
+let barmenu = document.getElementById("barmenu");
+let main_Menu=document.getElementById("main_menu");
+barmenu.addEventListener("click",function(){
+    if (main_menu.classList.contains("main-Menu")
+    main_menu.classList.remove("item-main-menu");
+    main_menu.classList.add("main-menu-visible");
+    )
+
+else{
+    main_Menu.classList.remove("main-menu-visible")
+    main_Menu.classList.add("main-menu-visible")
+}
+
+})

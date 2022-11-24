6c658ba92fec4ab3eabf803ea9a65760d13e6d58
jairola11 <jairostivenla11@gmail.com>
2022-11-24 10:06:46 -0500

    paisaje

--- js/media-query.js
+++ js/media-query.js
@@ -1,13 +1,13 @@
 let barMenu = document.getElementById("barMenu");
-let main_Menu = document.getElementById("mainMenu");
+let mainMenu = document.getElementById("mainMenu");
 
 barMenu.addEventListener("click",function() {
-    if (main_Menu.classList.contains("mainMenu")) {
-        main_Menu.classList.add("main-menu-visible");
-        main_Menu.classList.remove("mainMenu");
+    if (mainMenu.classList.contains("mainMenu")) {
+        mainMenu.classList.add("main-menu-visible");
+        mainMenu.classList.remove("mainMenu");
     } else {
-        main_Menu.classList.remove("main-menu-visible");
-        main_Menu.classList.add("mainMenu");
-    }; 
+        mainMenu.classList.remove("main-menu-visible");
+        mainMenu.classList.add("mainMenu");
+    };
 
 });

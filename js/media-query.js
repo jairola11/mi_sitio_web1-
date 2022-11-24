3f0f7786b05e4a305961d84026f5a54fb6ad1cc5
jairola11 <jairostivenla11@gmail.com>
2022-11-23 23:54:16 -0500

    menu

--- js/media-query.js
+++ js/media-query.js
@@ -1,14 +1,13 @@
-let barmenu = document.getElementById("barmenu");
-let main_Menu=document.getElementById("main_menu");
-barmenu.addEventListener("click",function(){
-    if (main_menu.classList.contains("main-Menu")
-    main_menu.classList.remove("item-main-menu");
-    main_menu.classList.add("main-menu-visible");
-    )
+let barMenu = document.getElementById("barMenu");
+let main_Menu = document.getElementById("mainMenu");
 
-else{
-    main_Menu.classList.remove("main-menu-visible")
-    main_Menu.classList.add("main-menu-visible")
-}
+barMenu.addEventListener("click",function() {
+    if (main_Menu.classList.contains("mainMenu")) {
+        main_Menu.classList.add("main-menu-visible");
+        main_Menu.classList.remove("mainMenu");
+    } else {
+        main_Menu.classList.remove("main-menu-visible");
+        main_Menu.classList.add("mainMenu");
+    }; 
 
-})
+});

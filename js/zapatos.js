7db1b926f1c97947e235fe4a659af860f28acba0
jairola11 <jairostivenla11@gmail.com>
2023-03-29 15:44:03 -0500

    js

---
+++ js/zapatos.js
@@ -0,0 +1,15 @@
+let imgprincipal = document.getElementById("imgprincipal");
+let  mainBox = document.getElementById(mainBox)
+imgprincipal.addEventListener("click",function(){
+    mainBox.classList.add("fondo-oscuro")
+});
+
+
+
+
+
+
+
+
+
+

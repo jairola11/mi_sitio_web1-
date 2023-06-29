3c089f934893f41ca41707dacb92945dc4ee2bd3
jairola11 <jairostivenla11@gmail.com>
2023-06-29 11:39:30 -0500

    cambios

---
+++ ejer-js/ej-js.js
@@ -0,0 +1,25 @@
+// activando el modo strict
+// "use strict"
+// nombreAprendiz= "jairo";
+// alert(nombreAprendiz);
+
+// alert("bienvenido al mundo de js")
+
+// ejer n3 crear dos variables  y mostrar por consola .....*/
+
+let nombreAprendiz ="jairo stiven ";
+let apellidoAprendiz=" lopez alvarez ";
+console.log( nombreAprendiz +"" + apellidoAprendiz);
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ js/fecha.js
@@ -0,0 +1,25 @@
+function calcularTiempoRestante() {
+    var dia = parseInt(document.getElementById("dia").value);
+    var mes = parseInt(document.getElementById("mes").value);
+    var anio = parseInt(document.getElementById("anio").value);
+    var hora = parseInt(document.getElementById("hora").value);
+    var minutos = parseInt(document.getElementById("minutos").value);
+  
+    var fechaActual = new Date();
+    var fechaLimite = new Date(anio, mes + 1, dia, hora, minutos);
+  
+    var diferencia = fechaLimite - fechaActual;
+  
+    if (diferencia < 0) {
+      document.getElementById("resultado").innerHTML = "El plazo ya ha vencido. No es posible subir el archivo.";
+    } else {
+      var diasRestantes = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+      diferencia -= diasRestantes * (1000 * 60 * 60 * 24);
+      var horasRestantes = Math.floor(diferencia / (1000 * 60 * 60));
+      diferencia -= horasRestantes * (1000 * 60 * 60);
+      var minutosRestantes = Math.floor(diferencia / (1000 * 60));
+  
+      document.getElementById("resultado").innerHTML = "Tiempo restante: " + diasRestantes + " días, " + horasRestantes + " horas y " + minutosRestantes + " minutos.";
+    }
+  }
+  

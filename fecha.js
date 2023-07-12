b448572dccb98ff85c11f256dc1fb92cd945d271
jairola11 <jairostivenla11@gmail.com>
2023-07-11 21:08:24 -0500

    cambios fecha

--- fecha.js
+++ fecha.js
@@ -1,25 +1,24 @@
 function calcularTiempoRestante() {
-    var dia = parseInt(document.getElementById("dia").value);
-    var mes = parseInt(document.getElementById("mes").value);
-    var anio = parseInt(document.getElementById("anio").value);
-    var hora = parseInt(document.getElementById("hora").value);
-    var minutos = parseInt(document.getElementById("minutos").value);
-  
-    var fechaActual = new Date();
-    var fechaLimite = new Date(anio, mes - 1, dia, hora, minutos);
-  
-    var diferencia = fechaLimite - fechaActual;
-  
-    if (diferencia < 0) {
-      document.getElementById("resultado").innerHTML = "El plazo ya ha vencido. No es posible subir el archivo.";
-    } else {
-      var diasRestantes = Math.floor(diferencia / (1000 * 60 * 60 * 24));
-      diferencia -= diasRestantes * (1000 * 60 * 60 * 24);
-      var horasRestantes = Math.floor(diferencia / (1000 * 60 * 60));
-      diferencia -= horasRestantes * (1000 * 60 * 60);
-      var minutosRestantes = Math.floor(diferencia / (1000 * 60));
-  
-      document.getElementById("resultado").innerHTML = "Tiempo restante: " + diasRestantes + " días, " + horasRestantes + " horas y " + minutosRestantes + " minutos.";
-    }
+  let dia = parseInt(document.getElementById("dia").value);
+  let mes = parseInt(document.getElementById("mes").value);
+  let anio = parseInt(document.getElementById("anio").value);
+  let hora = parseInt(document.getElementById("hora").value);
+  let minutos = parseInt(document.getElementById("minutos").value);
+
+  let fechaActual = new Date();
+  let fechaLimite = new Date(anio, mes - 1, dia, hora, minutos);
+
+  let diferencia = fechaLimite - fechaActual;
+
+  if (diferencia < 0 || fechaLimite.getDate() >= 22) {
+    document.getElementById("resultado").innerHTML = "El plazo ha expirado. No es posible subir el archivo.";
+  } else {
+    let diasRestantes = Math.floor(diferencia / (1000 * 60 * 60 * 24));
+    diferencia -= diasRestantes * (1000 * 60 * 60 * 24);
+    let horasRestantes = Math.floor(diferencia / (1000 * 60 * 60));
+    diferencia -= horasRestantes * (1000 * 60 * 60);
+    let minutosRestantes = Math.floor(diferencia / (1000 * 60));
+
+    document.getElementById("resultado").innerHTML = "Tiempo restante: " + diasRestantes + " días, " + horasRestantes + " horas y " + minutosRestantes + " minutos.";
   }
-  
+}

47699badd3cccd8cf93dabbc0deb9667a17bbddc
jairola11 <jairostivenla11@gmail.com>
2023-10-11 14:51:09 -0500

    mercadolibre

--- js/mercadolibre.js
+++ js/mercadolibre.js
@@ -1,100 +1,100 @@
-class Automovil{
-    constructor(marca,modelo,precio,anyo,ciudad,Kilometros){
-        this.marca= marca;
-        this.modelo=modelo;
-        this.precio=precio;
-        this.anyo=anyo;
-        this.ciudad=ciudad;
-        this.Kilometros=Kilometros;
+class Automovil {
+    constructor(marca, modelo, precio, anyo, ciudad, Kilometros, imagenURL) {
+        this.marca = marca;
+        this.modelo = modelo;
+        this.precio = precio;
+        this.anyo = anyo;
+        this.ciudad = ciudad;
+        this.Kilometros = Kilometros;
+        this.imagenURL = imagenURL;
     }
 }
 
-let auto1 = new Automovil("Toyota","Fortuner 2.8l", 325000000, 2023, "Neiva - Huila", 7600 );
+let auto1 = new Automovil("Toyota", "Fortuner 2.8l", 325000000, 2023, "Neiva - Huila", 7600, "img/toyota_fortuner.jpg");
+let auto2 = new Automovil("Toyota", "4Runner 4.0 Limited ", 290000000, 2022, "Bogotá - Cundinamarca", 29700, "img/4rrunerlimite.jpg");
 
 let inputBusqueda = document.getElementById("inputBusqueda");
 
-// inputBusqueda.addEventListener("load", function(event){
-window.addEventListener("load", function(){
-
-    // if(event.key == "Enter"){
+window.addEventListener("load", function() {
     let mainContent = document.getElementById("mainContent");
-    let boxAuto = document.createElement("div");
-    mainContent.appendChild(boxAuto);
-    boxAuto.setAttribute("class", "box-auto");
+    let automovilesMostrados = false;
 
-    //crear contenedor para la imagen//
+    function mostrarAutomovil(auto, imgURL) {
+        let boxAuto = document.createElement("div");
+        mainContent.appendChild(boxAuto);
+        boxAuto.setAttribute("class", "box-auto");
 
-    let boxImg = document.createElement("div");
+        let boxImg = document.createElement("div");
 
-        /*corazon imagen*/
-    let boxCorazon = this.document.createElement("div");
-    boxImg.appendChild(boxCorazon);
-    boxCorazon.setAttribute("class", "box-corazon")
-    let corazon = this.document.createElement("div");
-    boxCorazon.appendChild(corazon);
-    corazon.setAttribute("class", "corazon")
+        let boxCorazon = document.createElement("div");
+        boxImg.appendChild(boxCorazon);
+        boxCorazon.setAttribute("class", "box-corazon");
 
+        let corazon = document.createElement("div");
+        boxCorazon.appendChild(corazon);
+        corazon.setAttribute("class", "corazon");
 
-    /*cargando icono*/
-    let iconHearth = this.document.createElement("i");
-    corazon.appendChild(iconHearth);
-    iconHearth.setAttribute("class", "icons-style-hearth fa-regular fa-heart");
+        let iconHearth = document.createElement("i");
+        corazon.appendChild(iconHearth);
+        iconHearth.setAttribute("class", "icons-style-hearth fa-regular fa-heart");
 
-    boxAuto.appendChild(boxImg);
-    let imgAuto = document.createElement("img");
-    boxImg.appendChild(imgAuto);
-    boxImg.setAttribute("class", "box-img");
-    imgAuto.setAttribute("src", "img/toyota_fortuner.jpg")
-    imgAuto.setAttribute("class", "img-auto");
+        boxAuto.appendChild(boxImg);
 
+        let imgAuto = document.createElement("img");
+        boxImg.appendChild(imgAuto);
+        boxImg.setAttribute("class", "box-img");
+        imgAuto.setAttribute("src", imgURL);
+        imgAuto.setAttribute("class", "img-auto");
 
+        let boxInfo = document.createElement("div");
+        boxAuto.appendChild(boxInfo);
+        let infoMarca = document.createElement("div");
+        boxInfo.appendChild(infoMarca);
+        boxInfo.setAttribute("class", "box-info");
+        infoMarca.setAttribute("class", "info-marca");
+        let txtMarca = document.createElement("label");
+        infoMarca.appendChild(txtMarca);
+        txtMarca.innerText = auto.marca;
+        txtMarca.setAttribute("class", "txt-marca");
 
-    /*crear contenedor para la informacion*/
-    let boxInfo = document.createElement("div");
-    boxAuto.appendChild(boxInfo);
-    let infoMarca = this.document.createElement("div");
-    boxInfo.appendChild(infoMarca);
-    boxInfo.setAttribute("class", "box-info");
-    infoMarca.setAttribute("class", "info-marca");
-    let txtMarca = document.createElement("label");
-    infoMarca.appendChild(txtMarca);
-    txtMarca.innerText =auto1.marca;
-    txtMarca.setAttribute("class", "txt-marca")
+        let txtModelo = document.createElement("label");
+        infoMarca.appendChild(txtModelo);
+        txtModelo.innerText = " " + auto.modelo;
+        txtModelo.setAttribute("class", "txt-modelo");
 
-    let txtModelo = document.createElement("label");
-    infoMarca.appendChild(txtModelo);
-    txtModelo.innerText= " "+ auto1.modelo;
-    txtModelo.setAttribute("class", "txt-modelo")
+        let precioFormat = new Intl.NumberFormat("de-DE").format(auto.precio);
+        let txtPrecio = document.createElement("label");
+        boxInfo.appendChild(txtPrecio);
+        txtPrecio.innerHTML = "$" + precioFormat;
+        txtPrecio.setAttribute("class", "txt-precio");
 
-    /*precio*/
-    let precioFormat = new Intl.NumberFormat("de-DE").format(auto1.precio);
-    let txtPrecio = this.document.createElement("label");
-    boxInfo.appendChild(txtPrecio);
-    txtPrecio.innerHTML = "$" + precioFormat;
-    txtPrecio.setAttribute("class", "txt-precio")
-   
+        let boxProcedencia = document.createElement("div");
+        boxInfo.appendChild(boxProcedencia);
+        boxProcedencia.setAttribute("class", "box-procedencia");
 
-/*kilometraje y procedencia*/
-    let boxProcedencia = document.createElement("div");
-    boxInfo.appendChild(boxProcedencia);
-    boxProcedencia.setAttribute("class", "box-procedencia");
+        let txtanyo = document.createElement("label");
+        boxProcedencia.appendChild(txtanyo);
+        txtanyo.innerHTML = auto.anyo + " · ";
 
-    let txtanyo = document.createElement("label");
-    boxProcedencia.appendChild(txtanyo);
-    txtanyo.innerHTML = auto1.anyo + " · ";
+        let txtKilometros = document.createElement("label");
+        boxProcedencia.appendChild(txtKilometros);
+        let txtKilometrosFormat = new Intl.NumberFormat("de-DE").format(auto.Kilometros);
+        txtKilometros.innerHTML = txtKilometrosFormat + " · ";
 
-    let txtKilometros = document.createElement("label");
-    boxProcedencia.appendChild(txtKilometros);
-    let txtKilometrosFormat = new Intl.NumberFormat("de-DE").format(auto1.Kilometros);
-    txtKilometros.innerHTML = txtKilometrosFormat + " · ";
+        let txtciudad = document.createElement("label");
+        boxProcedencia.appendChild(txtciudad);
+        txtciudad.innerHTML = auto.ciudad;
 
-    let txtciudad = document.createElement("label");
-    boxProcedencia.appendChild(txtciudad);
-    txtciudad.innerHTML = auto1.ciudad;
+        let boxLineaDiv = document.createElement("div");
+        mainContent.appendChild(boxLineaDiv);
+        boxLineaDiv.setAttribute("class", "box-linea-div");
+    }
 
-    let boxLineaDiv = document.createElement("div");
-    mainContent.appendChild(boxLineaDiv);
-    boxLineaDiv.setAttribute("class", "box-linea-div");
-    
-}); 
- 
+    inputBusqueda.addEventListener("keyup", function(event) {
+        if (event.key === "Enter" && inputBusqueda.value.toLowerCase() === "toyota" && !automovilesMostrados) {
+            mostrarAutomovil(auto1, auto1.imagenURL);
+            mostrarAutomovil(auto2, auto2.imagenURL);
+            automovilesMostrados = true;
+        }
+    });
+});

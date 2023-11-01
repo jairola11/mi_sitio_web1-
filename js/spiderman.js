f2521c29e8a18381a93c94d8f19feaae8018fbf2
jairola11 <jairostivenla11@gmail.com>
2023-11-01 15:56:36 -0500

    spider-man

--- js/spiderman.js
+++ js/spiderman.js
@@ -2,11 +2,11 @@ let spiderman = document.getElementById('showSpiderman');
 let input = document.querySelector('input');
 
 input.addEventListener('keydown', (e) => {
-  if (e.key === 'Enter' && input.value.toLowerCase() === 'spiderman') {
+  if (e.key === 'Enter' && input.value.toLowerCase() === 'spider-man') {
     spiderman.classList.add('spiderman2');
     spiderman.classList.remove('spiderman');
   } else if (e.key === 'Backspace') {
     spiderman.classList.add('spiderman');
-    spiderman.classList.remove('spiderman2'); 
+    spiderman.classList.remove('spiderman2');
   }
 });

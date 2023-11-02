4a250224fd248ef0617927bb1f8ac1c08cd8ae50
jairola11 <jairostivenla11@gmail.com>
2023-11-02 08:52:20 -0500

    spiderman

--- js/spiderman.js
+++ js/spiderman.js
@@ -1,10 +1,21 @@
 let spiderman = document.getElementById('showSpiderman');
 let input = document.querySelector('input');
+let position = 5;
 
-input.addEventListener('keydown', (e) => {
-  if (e.key === 'Enter' && input.value.toLowerCase() === 'spider-man') {
-    spiderman.classList.add('spiderman2');
-    spiderman.classList.remove('spiderman');
+input.addEventListener('keyup', (e) => {
+  if (input.value.toLowerCase() === 'man') {
+    if (e.key === "ArrowLeft") {
+      position -= 3;
+    } else if (e.key === "ArrowRight") {
+      position += 3;
+    }
+
+    spiderman.style.left = position + "vw";
+
+    if (e.key === 'ArrowLeft' || e.key === 'ArrowRight') {
+      spiderman.classList.add('spiderman2');
+      spiderman.classList.remove('spiderman');
+    }
   } else if (e.key === 'Backspace') {
     spiderman.classList.add('spiderman');
     spiderman.classList.remove('spiderman2');

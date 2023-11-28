fdd66c46b354e540a96583ed2484eacba1ad0db0
jairola11 <jairostivenla11@gmail.com>
2023-11-28 13:40:41 -0500

    strellas

--- js/starCalculator.js
+++ js/starCalculator.js
@@ -38,56 +38,86 @@ function promStar(starsDistribution) {
     updateStar(averageStars * 20);
 }
 
+// function updateStar(valueStar) {
+//     const completStar = valueStar;
+
+
+
+//     const completStarprint = completStar / 20;
+//     const starfull = parseInt(completStarprint);
+//     const decimalStar = (completStarprint - starfull) * 10;
+
+//     let nextStar;
+//     if (completStar < 20) {
+//         Swal.fire({
+//             icon: 'info',
+//             title: '¡Ups!',
+//             text: 'Parece que no estás seguro. ¡Prueba de nuevo!',
+//         });;
+//     } else if (completStar >= 20 && completStar < 60) {
+//         Swal.fire({
+//             icon: 'error',
+//             title: '¡Oh no!',
+//             text: 'Tu opinión es valiosa, pero es una puntuación baja.',
+//         });
+//     } else if (completStar >= 60 && completStar < 80) {
+//         Swal.fire({
+//             icon: 'info',
+//             title: '¡Sigues mejorando!',
+//             text: 'Tu evaluación está en un buen camino.',
+//             timer: 2000,
+//             timerProgressBar: true,
+//             showConfirmButton: false,
+//         });
+//     } else if (completStar >= 80 && completStar < 100) {
+//         if (decimalStar < 8) {
+//             Swal.fire('Vas por buen camino');
+//         } else {
+//             Swal.fire({
+//                 icon: 'info',
+//                 title: '¡Casi perfecto!',
+//                 text: 'Continúa así, te acercas mucho.',
+//             });
+//         }
+//     } else if (completStar === 100) {
+//         Swal.fire({
+//             icon: 'success',
+//             title: '¡Eres increíble!',
+//             text: 'Gracias por la máxima puntuación. ¡Eres genial!',
+//             showConfirmButton: false,
+//             timer: 1500,
+//         });
+//     }
+
+//     stars.forEach((star, index) => {
+//         const fullStar = star.getAttribute('data-rating');
+//         star.classList.remove('star-active');
+//         star.classList.remove('star-decimal');
+
+//         if (index + 1 <= starfull) {
+//             star.classList.add('star-active');
+//         }
+//     });
+
+//     if (decimalStar >= 3 && decimalStar <= 7) {
+//         nextStar = stars[starfull];
+//         if (nextStar) {
+//             nextStar.classList.add('star-decimal');
+//         }
+//     }
+
+//     if (decimalStar > 7) {
+//         nextStar = stars[starfull];
+//         if (nextStar) {
+//             nextStar.classList.add('star-active');
+//         }
+//     }
+// }
 function updateStar(valueStar) {
     const completStar = valueStar;
-
-
-
     const completStarprint = completStar / 20;
     const starfull = parseInt(completStarprint);
     const decimalStar = (completStarprint - starfull) * 10;
-
-    let nextStar;
-    if (completStar < 20) {
-        Swal.fire({
-            icon: 'info',
-            title: '¡Ups!',
-            text: 'Parece que no estás seguro. ¡Prueba de nuevo!',
-        });;
-    } else if (completStar >= 20 && completStar < 60) {
-        Swal.fire({
-            icon: 'error',
-            title: '¡Oh no!',
-            text: 'Tu opinión es valiosa, pero es una puntuación baja.',
-        });
-    } else if (completStar >= 60 && completStar < 80) {
-        Swal.fire({
-            icon: 'info',
-            title: '¡Sigues mejorando!',
-            text: 'Tu evaluación está en un buen camino.',
-            timer: 2000,
-            timerProgressBar: true,
-            showConfirmButton: false,
-        });
-    } else if (completStar >= 80 && completStar < 100) {
-        if (decimalStar < 8) {
-            Swal.fire('Vas por buen camino');
-        } else {
-            Swal.fire({
-                icon: 'info',
-                title: '¡Casi perfecto!',
-                text: 'Continúa así, te acercas mucho.',
-            });
-        }
-    } else if (completStar === 100) {
-        Swal.fire({
-            icon: 'success',
-            title: '¡Eres increíble!',
-            text: 'Gracias por la máxima puntuación. ¡Eres genial!',
-            showConfirmButton: false,
-            timer: 1500,
-        });
-    }
 
     stars.forEach((star, index) => {
         const fullStar = star.getAttribute('data-rating');
@@ -100,14 +130,12 @@ function updateStar(valueStar) {
     });
 
     if (decimalStar >= 3 && decimalStar <= 7) {
-        nextStar = stars[starfull];
+        const nextStar = stars[starfull];
         if (nextStar) {
             nextStar.classList.add('star-decimal');
         }
-    }
-
-    if (decimalStar > 7) {
-        nextStar = stars[starfull];
+    } else if (decimalStar > 7) {
+        const nextStar = stars[starfull];
         if (nextStar) {
             nextStar.classList.add('star-active');
         }
